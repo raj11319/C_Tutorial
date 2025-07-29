6ff19044307f9a84cf0c25d3e41134b7052e3a3f
Raj Srivastava <raj113192007@gmail.com>
2025-07-29 12:40:09 +0530

    hu

---
+++ Name_age.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+    printf("My name is Raj \n");
+    printf("My age is 18 years \n");
+    return 0;
+}
--- Print_type.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main (){
-    int x ;
-    printf("Enter a number: ");
-    scanf("%d", & x);
-    printf("INTEGER IS %d", x);
-    return 0;
-   
-}
