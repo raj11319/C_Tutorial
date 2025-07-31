55f0eac03a4a7549fd673709a71665970341be29
Raj Srivastava <raj113192007@gmail.com>
2025-08-01 01:11:44 +0530

    Co-authored-by: shivambbu2000-crypto <shivambbu2000-crypto@users.noreply.github.com>

---
+++ 1to10_print.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    for(int i=1; i<=10; i=i+1){
+        printf("%d \n",i);
+    }
+    return 0;
+}
+// This code print numbers from 1 to 10
---
+++ ramesh_salary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int sal;
+    printf("enter sal \n");
+    scanf("%d", &sal);
+    printf("%d", sal); 
+    return 0;
+}
+
+
