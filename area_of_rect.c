143375510bf2527ae453fd4bc9b47961b4cf0224
raj11319 <raj113192007@gmail.com>
2025-10-08 18:50:53 +0530

    fgh

---
+++ area_of_rect.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int l, b;
+    printf("enter length \n", & l);
+    scanf("%d", & l);
+    printf("enter breadth \n", & b);
+    scanf("%d", & b);
+    printf("the area is %d \n", l * b );
+    printf("Thank you");
+    return 0;
+    
+}
+    //This code prints the area of rectangle..
