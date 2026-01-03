00f2cf41314c8836f1b4948b073671d6ec1fd7b7
raj11319 <raj113192007@gmail.com>
2026-01-03 09:43:57 +0530

    h

---
+++ reverse_a_no.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int a, b, c;
+    printf ("enter the values of  a, b and c\n");
+    scanf("%d %d %d", & a, &b, &c);
+    printf("the original forwad number is %d %d %d\n", a, b, c );
+    printf("the reverse number is %d %d %d \n", c, b, a);
+    printf("thank you");
+    return 0;
+
+}
+//this code reverse a number
